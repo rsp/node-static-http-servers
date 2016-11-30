899a93661a1316f2b49fb27adf829a377549bf1f
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-11-30 21:43:00 +0100

    add http version

---
+++ http.js
@@ -0,0 +1,58 @@
+// Copyright (c) 2016 Rafał Pocztarski
+// Released under MIT License (Expat) - see:
+// https://github.com/rsp/node-static-http-servers/blob/master/LICENSE.md
+
+var info = 'node-static-http-servers by Rafał Pocztarski\n'
+         + 'Examples of HTTP static file serving in Node.js\n'
+         + 'See: https://github.com/rsp/node-static-http-servers\n'
+         + 'Example using: http';
+
+var path = require('path');
+var http = require('http');
+var fs = require('fs');
+
+var dir = path.join(__dirname, 'public');
+
+var mime = {
+    html: 'text/html',
+    txt: 'text/plain',
+    css: 'text/css',
+    gif: 'image/gif',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    svg: 'image/svg+xml',
+    js: 'application/javascript'
+};
+
+var server = http.createServer(function (req, res) {
+    console.log(req);
+    var reqpath = req.url.toString().split('?')[0];
+    if (req.method !== 'GET') {
+        res.statusCode = 501;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Method not implemented');
+    }
+    var file = path.join(dir, reqpath.replace(/\/$/, '/index.html'));
+    if (file.indexOf(dir+'/') !== 0) {
+        res.statusCode = 403;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Forbidden');
+    }
+    var type = mime[path.extname(file).slice(1)] || 'text/plain';
+    var s = fs.createReadStream(file);
+    s.on('open', function () {
+        res.setHeader('Content-Type', type);
+        s.pipe(res);
+    });
+    s.on('error', function () {
+        res.setHeader('Content-Type', 'text/plain');
+        res.statusCode = 404;
+        res.end('Not found');
+    });
+});
+
+server.listen(3000, function () {
+    console.log('Listening on http://localhost:3000/');
+});
+
+console.log(info);
